ab93f91f65c0bda1e000808f5f5d1dc6df94da24
Joshua Krause <joshuakrause@t-online.de>
2022-02-16 14:07:07 +0100

    Defined LambdaTerm class

--- src/pupil_spline.cpp
+++ src/pupil_spline.cpp
@@ -18,6 +18,8 @@
 // Is implemented by a specific penalty (probably first identity).
 // Needs a public method to get the raw penalty matrix and the same matrix
 // parameterized with a lambda term.
+// These penalty terms are discussed extensively in Wood (2017)
+// Generalized Additive Models : An introduction with R, Second Edition
 class Penalty
 {
 protected:
@@ -67,3 +69,28 @@ Eigen::MatrixXd IdentityPenalty::parameterizePenalty(double l)
     Eigen::MatrixXd Sl = getPenalty().array() * l;
     return Sl;
 }
+
+// LambdaTerm class. As discussed in Wood (2017), multiple penalties (for individual
+// terms) can share the same lambda value (e.g., this is achieved with the 'by' keyword
+// in mgcv). Thus, the LambdaTerm class stores smart pointers to all the penalties
+// associated with this lambda value. It comes with a method to extract these penalties
+// (Not sure whether that is needed, just thought it might be handy..), a method to embedd these
+// penalties in a zero-padded matrix (S in Wood 2017) to represent them in quadratic form cf' * S * cf
+// where cf contains the weight for each column in the model matrix (Wood, 2017 s. 4.3.1).
+// This same method can also be used to embed multiple lambda terms in the same zero-padded matrix, which is
+// required for the EFS update described in Wood & Fasiolo (2017). The latter is also implemented
+// as a method here so that the lambda values of each term can be updated.
+class LambdaTerm
+{
+private:
+    std::vector<std::unique_ptr<Penalty>> penalties; // Penalties associated with lambdaterm
+    int nPenalties;                                  // Number of elements in penalties
+    double lambda;
+
+public:
+    LambdaTerm(int n, int dim);
+    const std::vector<std::unique_ptr<Penalty>> &getPenalties() const; // Will never change corresponding LambdaTerm instance.
+    void embeddInS(Eigen::MatrixXd &embS, int &cIndex);
+    void stepFellnerSchall(const Eigen::MatrixXd &embS, const Eigen::MatrixXd &cf, const Eigen::MatrixXd &inv,
+                           const Eigen::MatrixXd &gInv, int &cIndex, double sigma);
+};
