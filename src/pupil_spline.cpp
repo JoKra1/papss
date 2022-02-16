f9f82d8b0421bd9064bcdd866b542dcd06fcb30a
Joshua Krause <joshuakrause@t-online.de>
2022-02-16 13:40:40 +0100

    Removed template examples

---
+++ src/pupil_spline.cpp
@@ -0,0 +1,15 @@
+// -*- mode: C++; c-indent-level: 4; c-basic-offset: 4; indent-tabs-mode: nil; -*-
+
+// we only include RcppEigen.h which pulls Rcpp.h in for us
+#include <RcppEigen.h>
+#include <Eigen/Dense>
+#include <Eigen/QR>
+#include <vector>
+#include <memory>
+#include <cmath>
+#include <limits>
+
+// via the depends attribute we tell Rcpp to create hooks for
+// RcppEigen so that the build process will know what to do
+//
+// [[Rcpp::depends(RcppEigen)]]
