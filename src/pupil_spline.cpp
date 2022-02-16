c83cef6de3e0b76803ff941cc4ff8e6355e823f3
Joshua Krause <joshuakrause@t-online.de>
2022-02-16 13:44:04 +0100

    Defined penalty interface

--- src/pupil_spline.cpp
+++ src/pupil_spline.cpp
@@ -13,3 +13,19 @@
 // RcppEigen so that the build process will know what to do
 //
 // [[Rcpp::depends(RcppEigen)]]
+
+// Penalty abstract class (interface)
+// Is implemented by a specific penalty (probably first identity).
+// Needs a public method to get the raw penalty matrix and the same matrix
+// parameterized with a lambda term.
+class Penalty
+{
+protected:
+    int dim;
+
+public:
+    virtual Eigen::MatrixXd getPenalty() = 0;
+    virtual Eigen::MatrixXd parameterizePenalty(double l) = 0;
+    int getDim();
+};
+
