06b8d7151030a7782aab274847806104aa0d15ea
Joshua Krause <joshuakrause@t-online.de>
2022-02-17 16:08:50 +0100

    Implemented AGDT optimizer
    Also shortened the description for the EFS update, for details really Wood (2011, 2017) should be considered.

--- src/pupil_spline.cpp
+++ src/pupil_spline.cpp
@@ -14,9 +14,7 @@
 //
 // [[Rcpp::depends(RcppEigen)]]
 
-
 // ##################################### Classes #####################################
-
 
 // Penalty abstract class (interface)
 // Is implemented by a specific penalty (probably first identity).
@@ -160,27 +158,9 @@ void LambdaTerm::embeddInS(Eigen::MatrixXd &embS, int &cIndex)
 // and Wood (2011): 'Fast stable restricted maximum likelihood and marginal likelihood estimation
 // of semiparametric generalized linear models: Estimation of Semiparametric Generalized Linear Models'
 // the explicit calculation of (X' * X + embS)^-1 is undesirable. Thus we here invoke the update on
-// 'updated terms' (see below for a quick overview and Wood, 2011; Wood, 2017 for more details)
+// 'updated terms' (see code below for a quick overview and Wood, 2011; Wood, 2017 for more details)
 // obtained after repeated QR factorization and Cholesky decomposition, as described extensively
-// in Wood (2011, 2017) to improve on the ill-conditioned nature of the former term. Wood (2017) shows
-// that X can first be decomposed into X = Q * R. By obtaining f = t(Q) * y, the normal
-// solution to a least squares problem cf = (X' * X)^-1 * X' * y can be re-expressed in terms of
-// R and f as cf = (R)^-1 * f, allowing to consider the least squares problem purely in terms of R and f.
-// In case of a penalized least squares problem (as the one considered here), involving a penalty
-// matrix like embS, Wood (2011, 2017) shows that after first obtaining the Cholesky factor cholS of
-// embS = cholS' * cholS and then forming a second QR decomposition of the row-wise concatenation [R,cholS] = Q2 * R2,
-// R2 corresponds to the root of X' * X + embS - thus yielding (X' * X + embS)^-1 = (R2)^-1 * (R2)^-1'
-// (Note that additional updates to f are necessary, involving Q and Q2, see: Wood, 2017).
-// Thus, the update rule as discussed in Wood & Fasiolo (2017):
-// lambda_i+1 = sigma^2 * ((tr(ginv(embS) * embJ) - tr((X' * X + embS)^-1) * embJ) / (cf' * embJ * cf)) * lambda_i
-// is here calculated as:
-// lambda_i+1 = sigma^2 * ((tr(ginv(embS) * embJ) - tr((R2)^-1 * (R2)^-1' * embJ) / (cf' * embJ * cf)) * lambda_i
-// Where tr() denotes the trace calculation and ginv() corresponds to forming the generalized MP. inverse.
-// Similarly, instead of calculating sigma^2 as:
-// sigma^2 = ((y - X * cf).(y - X * cf)) / (n - tr((X' * X + embS)^-1 * X` * X))
-// We here calculate sigma^2 as:
-// sigma^2 = ((f - R * cf).(f - R * cf) + ((y).(y) - (f).(f))) / (n - tr((R2)^-1 * (R2)^-1' * R' * R))
-// Here n is the number of rows in X and ().() refers to the dot product between the vectors in () and ().
+// in Wood (2011, 2017) to improve on the ill-conditioned nature of the former term.
 void LambdaTerm::stepFellnerSchall(const Eigen::MatrixXd &embS, const Eigen::MatrixXd &cf, const Eigen::MatrixXd &inv,
                                    const Eigen::MatrixXd &pInv, int &cIndex, double sigma)
 {
@@ -189,7 +169,6 @@ void LambdaTerm::stepFellnerSchall(const Eigen::MatrixXd &embS, const Eigen::Mat
     int dimS = embS.rows();
     Eigen::MatrixXd embJ = Eigen::MatrixXd::Zero(dimS, dimS);
     this->embeddInS(embJ, cIndex);
-
 
     // Calculate Numerator and Demoninator of the FS update, as described above.
     double num = (pInv * embJ).trace() - (inv * embJ).trace();
@@ -221,5 +200,75 @@ void enforceConstraints(Eigen::VectorXd &cf, const std::vector<char> &constraint
             }
         }
         ++idx;
+    }
+}
+
+// Gradient descent optimizer with momentum and restarts and Paul Tseng's momentum update
+// rule that allows to optimize a penalized least squares problem.
+// Further permits for optimizing a penalized Non-negative least squares problem
+// (penalized NNLS) by means of projection (see link below for details).
+// For discussion of algorithm see: https://angms.science/doc/NMF/nnls_pgd.pdf
+// code is based on the algorithm/pseudo-code discussed in aforementioned
+// source and has been adapted to solve a penalized NNLS instead of a simple
+// NNLS. Gradient of the penalized least squares loss function is from
+// Wood (2017).
+void agdTOptimize(Eigen::VectorXd &cf, const Eigen::MatrixXd &R, const Eigen::VectorXd &f,
+                  const Eigen::MatrixXd &embS, const std::vector<char> &constraints, double r,
+                  int maxiter, double tol)
+{
+    // Notation follows https://angms.science/doc/NMF/nnls_pgd.pdf.
+    Eigen::MatrixXd Rt = R.transpose();
+    Eigen::MatrixXd Q = Rt * R;
+    Eigen::MatrixXd p = Rt * f;
+
+    // Pre-calculate learning rate.
+    Eigen::MatrixXd QQ = Q.array() * Q.array(); // Element wise Q_i**2
+    double lr = 1 / sqrt(QQ.sum());
+
+    // Add Penalty to R term.
+    Q += embS;
+
+    // Calculate momentum related coefficients.
+    Eigen::VectorXd ycf0 = Eigen::VectorXd(cf);
+    Eigen::VectorXd ycf = ycf0;
+    Eigen::VectorXd prevCf = ycf0;
+
+    // Error increase check.
+    double prevErr = std::numeric_limits<double>::max();
+
+    for (int i = 0; i < maxiter; ++i)
+    {
+        // Take an accelerated gradient step.
+        cf = ycf - (lr * ((Q * ycf) - p));
+
+        // Enforce constraints.
+        enforceConstraints(cf, constraints);
+
+        // Calculate momentum.
+        double bk = (i) / (i + 3);
+        ycf = cf + bk * (cf - prevCf);
+
+        // Error calculation.
+        Eigen::VectorXd err = f - R * cf;
+        double errDot = err.dot(err);
+
+        // Restart handling.
+        if (errDot > prevErr)
+        {
+            cf = prevCf - (lr * ((Q * prevCf) - p));
+            enforceConstraints(cf, constraints);
+            ycf = cf;
+        }
+
+        // Crude convergence check.
+        double absErrDiff = errDot > prevErr ? errDot - prevErr : prevErr - errDot;
+        if (absErrDiff < tol)
+        {
+            break;
+        }
+
+        // Prepare next iter.
+        prevCf = cf;
+        prevErr = errDot;
     }
 }
