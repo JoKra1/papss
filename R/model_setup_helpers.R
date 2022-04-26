4ef9fa81eba02dc8ea169c45cc628b600f2d744a
Joshua Krause <52180639+JoKra1@users.noreply.github.com>
2022-04-26 15:18:51 +0200

    Added dual intercept expansion model

--- R/model_setup_helpers.R
+++ R/model_setup_helpers.R
@@ -131,6 +131,26 @@ term_by_factor <- function(term,fact) {
   return(term_by)
 }
 
+expand_var_by_rep <- function(var, fact, by) {
+  expanded_var <- c()
+  for (l in unique(fact)) {
+    fact_var <- var[fact == l]
+    expanded_var <- c(expanded_var,rep(fact_var[1],times=by),fact_var)
+  }
+  return(expanded_var)
+}
+
+expand_var_by_seq <- function(var, fact, from, step=20) {
+  expanded_var <- c()
+  for (l in unique(fact)) {
+    fact_var <- var[fact == l]
+    expanded_var <- c(expanded_var,
+                      (seq(-from,(fact_var[1] - step),by=step) + from),
+                      (fact_var + from))
+  }
+  return(expanded_var)
+}
+
 ### PAPSS model templates ###
 
 #' Creates trainings matrix and penalties for a fully penalized
@@ -305,6 +325,91 @@ WIER_DEN_SHARED_NNLS_model_setup <- function(time,subs,pulse_locations,n,t_max,f
               "constraints"=constraints))
 }
 
+#' Creates trainings matrix and penalties for a fully penalized
+#' model that combines elements of both the models by Denison et al., (2020)
+#' and Wierda et al. (2012).
+#'
+#' Like by Wierda et al. (2012) separate sets of coefficients are estimated per
+#' subject with the h_basis terms. All those coefficients are again constrained
+#' to be non-negative. Model also includes the intercept terms for each subject that
+#' Denison et al. (2012) introduced as well as the slope term introduced by
+#' Wierda et al., (2012). These are not constrained - since their
+#' primary purpose was to account for pupil trajectories that were overall just
+#' containing very negative samples (relative to baseline) and drifts in the
+#' trajectories respectively.
+#' 
+#' This model differs from the one by Wierda et al. (2012) and Denison et al., (2020)
+#' in that it penalizes the coefficients corresponding to the h_basis terms.
+#' Specifically, it enforces a single penalty term shared by all subjects (e.g., this is
+#' similar to the 'fs' basis in mgcv or can be achieved by using the 'id' keyword
+#' in a 'by' factor smooth.). The form of the penalty expressed on all of the
+#' basis functions is a simple identity matrix. We here also penalize all intercept
+#' and slope terms, again with a single penalty
+#' (this time applied to a single matrix though).
+#' 
+#' @param time A numeric vector containing positive time values in ms
+#' @param subs A factor vector containing subject identifiers
+#' @param pulse_locations A numeric vector containing index values of pulse loc.
+#' @param n Parameter defined by Hoeks & Levelt (number of laters)
+#' @param t_max Parameter defined by Hoeks & Levelt (response maximum in ms)
+#' @param f Parameter defined by Wierda et al. (scaling factor)
+WIER_DEN_SHARED_NNLS_EXP_model_setup <- function(expanded_time,time,subs,pulse_locations,n,t_max,f,expand_by,step) {
+  
+  # Extract number of subjects
+  n_subs <- length(unique(subs))
+  
+  # Create expansion index vector
+  original_index <- expanded_time >= expand_by
+
+  # Setup model matrix
+  
+  # Two intercepts, one for original period, one for expansion
+  intercept <- create_constant_term(expanded_time)
+  intercept_expansion <- intercept
+  intercept_original <- intercept
+  
+  intercept_expansion[original_index] = 0
+  intercept_original[!original_index] = 0
+  
+  intercept_expansion_by = term_by_factor(intercept_expansion,subs)
+  intercept_original_by = term_by_factor(intercept_original,subs)
+  
+  
+  # Slope, only over original period
+  slope <- create_slope_term(unique(expanded_time),n_subs)
+  slope[slope <= (expand_by/step)] <- 0
+  slope[slope > (expand_by/step)] <- slope[slope > (expand_by/step)] - (expand_by/step)
+  slope_matrix <- term_by_factor(slope,subs)
+  
+  # Spike matrix, over entire period
+  spike_matrix <- create_spike_matrix_term(expanded_time,pulse_locations,n,t_max,f)
+  spike_matrix_by <- term_by_factor(spike_matrix,subs)
+  
+  trainingsMatrix <- cbind(intercept_expansion_by,
+                           intercept_original_by,
+                           slope_matrix,
+                           spike_matrix_by)
+  
+  # Setup Penalty definition to be implemented by c++
+  # two individual intercept  and slope penalties plus one
+  # shared penalty for all subjects (expressed on bases)
+  freq <- c(1, 1, 1, n_subs)
+  # individual penalties are of size n_subs*n_subs. Also, Each of the shared
+  # penalties is of size length(pulse_locations)*length(pulse_locations)
+  size <- c(n_subs, n_subs, n_subs, length(pulse_locations))
+  
+  
+  # Define positive constraints
+  constraints <- rep("c",ncol(trainingsMatrix))
+  constraints[1:(3 * n_subs)] <- "u" # All parametric terms are unconstrained
+  
+  return(list("X"=trainingsMatrix,
+              "Penalties"=list("freq"=freq,
+                               "size"=size,
+                               "startIndex"=0),
+              "constraints"=constraints))
+}
+
 #' The main wrapper function that is also exposed. Fits the desired model.
 #' @export
 pupil_solve <- function(pulse_locations,real_locations,
@@ -315,10 +420,21 @@ pupil_solve <- function(pulse_locations,real_locations,
                         convergence_tol=1e-08,
                         should_collect_progress=F,
                         start_lambda=0.1,
-                        init_cf = NULL) {
+                        init_cf = NULL,
+                        expand_by = 1000,
+                        sample_len=20) {
   
   
+  # create optional variables for expansion
+  expanded_time <- NULL
+  expanded_subs <- NULL
+  expanded_pupil <- NULL
   
+  # Create y vector
+  y <- matrix(nrow = length(data$pupil),ncol=1)
+  y[,1] <- data$pupil
+  
+  # Create model setup
   if (model == "WIER_SHARED") {
     # Wierda et al. (2012) model, but with shared penalty!
     setup <- WIER_SHARED_NNLS_model_setup(data$time,data$subject,
@@ -334,6 +450,31 @@ pupil_solve <- function(pulse_locations,real_locations,
     setup <- WIER_DEN_SHARED_NNLS_model_setup(data$time,data$subject,
                                               pulse_locations,
                                               n,t_max,f)
+  } else if (model == "WIER_DEN_SHARED_EXP") {
+    # Combined model with shared penalty and baseline expansion!
+    
+    # Expand time and subject
+    expanded_time <- expand_var_by_seq(data$time,data$subject,expand_by,sample_len)
+    
+    expanded_subs <- as.factor(expand_var_by_rep(as.numeric(data$subject),data$subject,(expand_by/sample_len)))
+    
+    # Re-calculate y vector
+    expanded_pupil <- expand_var_by_rep(data$pupil,data$subject,(expand_by/sample_len))
+    y <- matrix(nrow = length(expanded_pupil),ncol=1)
+    y[,1] <- expanded_pupil
+    
+    # Re-calculate pulse locations
+    pulse_locations_diff <- pulse_locations[2] - pulse_locations[1]
+    last_pulse <- length(unique(expanded_time)) - (3 * round(930/100)) - 1
+    pulse_locations <- seq(1,last_pulse,pulse_locations_diff)
+    real_locations <- unique(expanded_time)[pulse_locations]
+    
+    # Now proceed with setup
+    setup <- WIER_DEN_SHARED_NNLS_EXP_model_setup(expanded_time,
+                                                  data$time,expanded_subs,
+                                                  pulse_locations,
+                                                  n,t_max,f,expand_by,
+                                                  sample_len)
   } else {
     stop("Model not specified.")
   }
@@ -343,11 +484,6 @@ pupil_solve <- function(pulse_locations,real_locations,
     init_cf <- matrix(nrow=ncol(setup$X),ncol=1)
     init_cf[,1] <- runif(n=ncol(setup$X))
   }
-  
-  
-  # Create y vector
-  y <- matrix(nrow = length(data$pupil),ncol=1)
-  y[,1] <- data$pupil
 
   solved_pup <- wrapAmSolve(setup$X,y,init_cf,setup$constraints,
                             setup$Penalties$freq,setup$Penalties$size,
@@ -360,5 +496,10 @@ pupil_solve <- function(pulse_locations,real_locations,
               "lambdas" = solved_pup$finalLambdas,
               "sigma" = solved_pup$sigma,
               "convergence" = solved_pup$convergence,
-              "coefHistory" = solved_pup$coefChanges))
+              "coefHistory" = solved_pup$coefChanges,
+              "usedPulseLocations"=pulse_locations,
+              "usedRealLocations"=real_locations,
+              "expandedPupil"=expanded_pupil,
+              "expandedTime"=expanded_time,
+              "expandedSubjects"=expanded_subs))
 }
